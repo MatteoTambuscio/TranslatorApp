626f8c4f71cf93b77b30c8ede942a397c39a73cb
MatteoTambuscio <81351348+MatteoTambuscio@users.noreply.github.com>
2022-08-26 18:14:07 +0200

    Update trueCaser.py

--- trueCaser.py
+++ trueCaser.py
@@ -3,8 +3,8 @@ import stanza
 # processors={"pos":"combined"}
 #stanza.download('en', processors=["pos", "tokenize", "mwt"], model_dir='resources_en/') #processors={"tokenize":"mwt"}, model_dir='resources_en/')
 #stanza.download('it', processors="pos", model_dir='resources_it/') #processors="tokenize", package="ewt", model_dir='resources_it/')
-nlp = { "IT": stanza.Pipeline('it', model_dir='https://github.com/MatteoTambuscio/TranslatorApp/tree/main/resources_en', processors='pos,tokenize,mwt', download_method=None),
-        "EN": stanza.Pipeline('en', model_dir='resources_en/', processors='pos,tokenize,mwt', download_method=None) }
+nlp = { "IT": stanza.Pipeline('it', model_dir='https://github.com/MatteoTambuscio/TranslatorApp/tree/main/resources_iy', processors='pos,tokenize,mwt', download_method=None),
+        "EN": stanza.Pipeline('en', model_dir='https://github.com/MatteoTambuscio/TranslatorApp/tree/main/resources_em', processors='pos,tokenize,mwt', download_method=None) }
 
 def true_text(text, target_len):
 
