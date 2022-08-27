ad7ac455c6e2772db565f9ee3ff8a78ea71e6627
MatteoTambuscio <81351348+MatteoTambuscio@users.noreply.github.com>
2022-08-27 17:03:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -57,8 +57,8 @@ def translate(source, translator, sp_source_model, sp_target_model):
 @st.cache(allow_output_mutation=True)
 def load_models(source_len, target_len, domain):
     ct_model_path = "model/"+domain+"/"+source_len+"_"+target_len
-    sp_source_model_path = "model/vocab_model"+source_len+".model"
-    sp_target_model_path = "model/vocab_model"+target_len+".model"
+    sp_source_model_path = "model/vocab_model/"+source_len+".model"
+    sp_target_model_path = "model/vocab_model/"+target_len+".model"
 
     translator = ctranslate2.Translator(ct_model_path)
     sp_source_model = spm.SentencePieceProcessor(sp_source_model_path)
